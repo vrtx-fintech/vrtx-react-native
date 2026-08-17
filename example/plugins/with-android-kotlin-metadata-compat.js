b20ba214c95e4a353e07130813b6688bf91bf6d0
AbdelMonaam Aouini <clemonaam@gmail.com>
2026-08-17 13:59:20 +0100

    fix: build failure android

--- example/plugins/with-android-kotlin-metadata-compat.js
+++ example/plugins/with-android-kotlin-metadata-compat.js
@@ -9,11 +9,21 @@ subprojects { subproject ->
   }
 }
 `;
+const FREE_RASP_REPOSITORY =
+  'https://europe-west3-maven.pkg.dev/talsec-artifact-repository/freerasp';
 
 module.exports = function withAndroidKotlinMetadataCompat(config) {
   return withProjectBuildGradle(config, (config) => {
     if (!config.modResults.contents.includes('-Xskip-metadata-version-check')) {
       config.modResults.contents += `\n${KOTLIN_METADATA_COMPAT_BLOCK}\n`;
+    }
+
+    if (!config.modResults.contents.includes(FREE_RASP_REPOSITORY)) {
+      config.modResults.contents = config.modResults.contents.replace(
+        /allprojects\s*\{\s*repositories\s*\{/,
+        (match) =>
+          `${match}\n    maven { url '${FREE_RASP_REPOSITORY}' }\n    maven { url 'https://jitpack.io' }`,
+      );
     }
 
     return config;
